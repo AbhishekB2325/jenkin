3cc2dcafde99041b22f4e335bd65c6f6df7cf38b
AbhishekB2325 <abhishekbodda.22.it@anits.edu.in>
2025-01-20 11:28:43 +0530

    Update abc.java

--- abc.java
+++ abc.java
@@ -3,6 +3,7 @@ public class abc{
     public static void main(String args[]){
         int a=10;
         int b=20;
-        System.out.println("sum=",a+b);
+        int c=a+b
+        System.out.println("sum="+ c);
     }
 }
